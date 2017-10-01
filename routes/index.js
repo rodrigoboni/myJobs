2e3c1fe7764ca3a7cf97d35ae40be66159a95656
Rodrigo <rodrigoboni@gmail.com>
2017-10-01 16:16:53 -0300

    index route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,120 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.status(200).json({ message: 'index' });
+  res.status(200).json({
+    avaliableRoutes: [
+      {
+        route: 'employers',
+        endpoints: [
+          {
+            description: 'Get all employers',
+            uri: '/employers/',
+            method: 'GET',
+          },
+          {
+            description: 'Get a employer',
+            uri: '/employers/:id',
+            method: 'GET',
+            uriParameters: [
+              { id: { type: 'Number' } }
+            ],
+          },
+          {
+            description: 'Create a employer',
+            uri: '/employers/',
+            method: 'POST',
+            body: {
+              name: { type: 'String', required: true },
+              city: { type: 'String', required: true },
+              obs: { type: 'String' },
+            },
+          },
+          {
+            description: 'Update a employer',
+            uri: '/employers/:id',
+            method: 'PUT',
+            uriParameters: [
+              { id: { type: 'Number' } }
+            ],
+            body: {
+              name: { type: 'String', required: true },
+              city: { type: 'String', required: true },
+              obs: { type: 'String' },
+            },
+          },
+          {
+            description: 'Delete a employer',
+            uri: '/employers/:id',
+            method: 'DELETE',
+            uriParameters: [
+              { id: { type: 'Number' } }
+            ],
+          },
+        ],
+      },
+
+      {
+        route: 'jobs',
+        endpoints: [
+          {
+            description: 'Get all jobs',
+            uri: '/jobs/',
+            method: 'GET',
+          },
+          {
+            description: 'Get a job',
+            uri: '/jobs/:id',
+            method: 'GET',
+            uriParameters: [
+              { id: { type: 'Number' } }
+            ],
+          },
+          {
+            description: 'Create a job',
+            uri: '/job/',
+            method: 'POST',
+            body: {
+              title: { type: 'String', required: true },
+              description: { type: 'String', required: true },
+              _employer: { type: 'Schema.Types.ObjectId', required: true, ref: 'employers' },
+              weekdays: { type: 'Boolean', default: true },
+              weekends: { type: 'Boolean', default: false },
+              hoursDay: { type: 'Number', required: true },
+              wage: { type: 'Number', required: true },
+              previousExperience: { type: 'Boolean', default: false },
+              obs: { type: 'String' },
+            },
+          },
+          {
+            description: 'Update a job',
+            uri: '/jobs/:id',
+            method: 'PUT',
+            uriParameters: [
+              { id: { type: 'Number' } }
+            ],
+            body: {
+              title: { type: 'String', required: true },
+              description: { type: 'String', required: true },
+              _employer: { type: 'Schema.Types.ObjectId', required: true, ref: 'employers' },
+              weekdays: { type: 'Boolean', default: true },
+              weekends: { type: 'Boolean', default: false },
+              hoursDay: { type: 'Number', required: true },
+              wage: { type: 'Number', required: true },
+              previousExperience: { type: 'Boolean', default: false },
+              obs: { type: 'String' },
+            },
+          },
+          {
+            description: 'Delete a job',
+            uri: '/jobs/:id',
+            method: 'DELETE',
+            uriParameters: [
+              { id: { type: 'Number' } }
+            ],
+          },
+        ],
+      }
+  ] });
 });
 
 module.exports = router;
